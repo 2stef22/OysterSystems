0a3f579737b869b1e455dbfbbc4e82d68789db9d
mismaria <marmisc98@gmail.com>
2017-12-04 17:52:42 +0000

    Changes to journeyEventTeat

--- Oyster (1)/Oyster/src/com/tfl/billing/JourneyEventTest.java
+++ Oyster (1)/Oyster/src/com/tfl/billing/JourneyEventTest.java
@@ -7,73 +7,62 @@ import java.util.UUID;
 import org.junit.Test;
 
 public class JourneyEventTest {
+	UUID customerCardId = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+	UUID stationReaderIdStart = UUID.randomUUID();
+	UUID stationReaderIdEnd = UUID.randomUUID();
+
+	public JourneyEvent createJourneyStart() {
+		JourneyEvent myJourneyEventStart = new JourneyStart(customerCardId, stationReaderIdStart);
+		return myJourneyEventStart;
+
+	}
+
+	public JourneyEvent createJourneyEnd() {
+		JourneyEvent myJourneyEventEnd = new JourneyEnd(customerCardId, stationReaderIdEnd);
+		return myJourneyEventEnd;
+	}
+
+	@Test
+	public void TestCardIdStart() {
+		JourneyEvent myJourneyEventStart = createJourneyStart();
+		assertEquals(myJourneyEventStart.cardId(), customerCardId);
+	}
+
+	@Test
+	public void TestReaderIdStart() {
+		JourneyEvent myJourneyEventStart = createJourneyStart();
+		assertEquals(myJourneyEventStart.readerId(), stationReaderIdStart);
+
+	}
+
+	@Test
+	public void TestCardIdEnd() {
+		JourneyEvent myJourneyEventEnd = createJourneyEnd();
+		assertEquals(myJourneyEventEnd.cardId(), customerCardId);
+	}
+
+	@Test
+	public void TestReaderIdEnd() {
+		JourneyEvent myJourneyEventEnd = createJourneyEnd();
+		assertEquals(myJourneyEventEnd.readerId(), stationReaderIdEnd);
 
 
+	}
+
+/*
+    @Test
+    public void JourneyEventIsCreated() {
+
+        stationReaderId = UUID.randomUUID();
+        UUID aCardId = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+
+        JourneyEvent myJourneyEvent = new JourneyStart(aCardId, stationReaderId);
 
 
+        assertEquals(myJourneyEvent.cardId(), aCardId);
+        assertEquals(myJourneyEvent.readerId(), stationReaderId);
 
-	 UUID customerCardId = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-	    UUID stationReaderIdStart = UUID.randomUUID();
-	    UUID stationReaderIdEnd = UUID.randomUUID();
-
-	    JourneyEvent myJourneyEvent1 = new JourneyStart(customerCardId, stationReaderIdStart);
-	    JourneyEvent myJourneyEvent2 = new JourneyEnd(customerCardId, stationReaderIdEnd);
-	    Journey myJourney = new Journey(myJourneyEvent1, myJourneyEvent2);
-
-	   @Test
-	    public void TestOriginId() {
-	        assertEquals(myJourney.originId(), stationReaderIdStart);
-	       assertEquals(myJourney.startcustomerId(), customerCardId);
-	    }
-
-	    @Test
-	    public void TestDestinationId() {
-	        assertEquals(myJourney.destinationId(), stationReaderIdEnd);
-	        assertEquals(myJourney.endcustomerId(), customerCardId);
-	    }
-
-
-
-	    @Test
-	    public void JourneyEventIsCreated() {
-	        UUID customerCardId, stationReaderId;
-	        //OysterCard myCardTest = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-	        stationReaderId = UUID.randomUUID();
-
-	        UUID aCardId = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-
-	        JourneyEvent myJourneyEvent = new JourneyStart(aCardId, stationReaderId);
-
-
-	        assertEquals(myJourneyEvent.cardId(), aCardId);
-	        assertEquals(myJourneyEvent.readerId(), stationReaderId);
-
-	    }
-
-
-
-
-	   @Test
-
-	    public void JourneyIsCompleted()
-	    {
-	        UUID customerCardId , stationReaderIdStart,stationReaderIdEnd;
-	        //OysterCard myCardTest = new OysterCard("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-	        stationReaderIdStart =UUID.randomUUID();
-	        stationReaderIdEnd =UUID.randomUUID();
-
-	        UUID aCardId = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
-
-	        JourneyEvent myJourneyEvent1 = new JourneyStart(aCardId, stationReaderIdStart);
-	        JourneyEvent myJourneyEvent2 = new JourneyEnd(aCardId, stationReaderIdEnd);
-
-	        assertEquals(myJourneyEvent1.cardId(), aCardId);
-	        assertEquals(myJourneyEvent1.readerId(), stationReaderIdStart);
-	        assertEquals(myJourneyEvent2.cardId(), aCardId);
-	        assertEquals(myJourneyEvent2.readerId(), stationReaderIdEnd);
-
-
-	    }
-	   
-	   
+    }
+    */
 }
+
